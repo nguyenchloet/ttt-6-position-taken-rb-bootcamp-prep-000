dca72e2581c3b84981ae4b12e410b896b1380cfe
Tien Nguyen <pri859@sudomail.com>
2018-05-31 16:05:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ index = 0
 def position_taken?(board, index)
 if index == "" || index == " "
   return false
-elsif index = nil
+elsif index == nil
   return false
 else index == "X" || index == "O"
   return true
